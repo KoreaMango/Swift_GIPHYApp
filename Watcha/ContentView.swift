91cd50b535c16efbda370202815f676e13f47ea1
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-25 08:05:11 +0900

    Initial Commit

---
+++ Watcha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watcha
+//
+//  Created by 강민규 on 2022/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watcha/WatchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchaApp.swift
+//  Watcha
+//
+//  Created by 강민규 on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
