c5038adb584ae1f1bf2319913ca2548d98c04003
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-25 12:38:38 +0900

    Model User Write

--- Watcha/Models/User.swift
+++ Watcha/Models/User.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 struct User{
-    
+    var avatar_url: String
+    var banner_url: String
+    var profile_url: String
+    var username: String
+    var display_name: String
 }
