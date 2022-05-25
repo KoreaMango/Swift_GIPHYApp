295cf4afbdb4201d312d5787d4a896f39d8d5eb6
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-25 12:36:17 +0900

    Model Images Write

--- Watcha/Models/Images.swift
+++ Watcha/Models/Images.swift
@@ -8,5 +8,154 @@
 import Foundation
 
 struct Images {
+    struct fixed_height {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var mp4: String
+        var mp4_size: String
+        var webp: String
+        var webp_size: String
+        
+    }
+    struct fixed_height_still {
+        var url: String
+        var width: String
+        var height: String
+        
+    }
+    struct fixed_height_downsampled {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var webp: String
+        var webp_size: String
+        
+    }
+    struct fixed_width {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var mp4: String
+        var mp4_size: String
+        var webp: String
+        var webp_size: String
+        
+    }
+    struct fixed_width_still {
+        var url: String
+        var width: String
+        var  height: String
+    }
+    struct fixed_width_downsampled {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var webp: String
+        var webp_size: String
+        
+    }
+    
+    struct fixed_height_small {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var mp4: String
+        var mp4_size: String
+        var webp: String
+        var webp_size: String
+    }
+    
+    struct fixed_height_small_still {
+        var url: String
+        var width: String
+        var height: String
+    }
+    
+    struct fixed_width_small {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+        var mp4: String
+        var mp4_size: String
+        var webp: String
+        var webp_size: String
+    }
+    
+    struct fixed_width_small_still {
+        var url: String
+        var width: String
+        var height: String
+    }
+    
+    struct downsized {
+        var url: String
+        var width: String
+        var height: String
+    }
+    
+    struct downsized_large {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+    }
+    
+    struct downsized_medium {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+    }
+    
+    struct downsized_small {
+        var url: String
+        var width: String
+        var height: String
+        var size: String
+    }
+    
+    struct original {
+        var width: String
+        var height: String
+        var size: String
+        var frames: String
+        var mp4: String
+        var mp4_size: String
+        var webp: String
+        var webp_size: String
+        
+    }
+    struct original_still {
+        var url: String
+        var width: String
+        var height: String
+    }
+    
+    struct looping {
+        var mp4: String
+        
+    }
+    
+    struct preview {
+        var mp4: String
+        var mp4_size: String
+        var width: String
+        var height: String
+        
+    }
+    
+    struct preview_gif {
+        var url: String
+        var width: String
+        var height: String
+        
+    }
     
 }
