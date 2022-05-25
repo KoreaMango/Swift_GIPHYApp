1bd01a6c38823d424d3672ab6edfc63f69e8939a
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-25 15:57:04 +0900

    URL Setting

---
+++ Watcha/Constants/URL.swift
@@ -0,0 +1,17 @@
+//
+//  URL.swift
+//  Watcha
+//
+//  Created by 강민규 on 2022/05/25.
+//
+
+import Foundation
+
+struct URL {
+    static let serverURL : String = "api.giphy.com/v1"
+    
+    enum nextURL{
+        static let gifURL : String = "/gifs/search"
+        static let Sticker : String = "/stickers/search"
+    }
+}
