ede33b62d38f0393d2816aa8f09d64ffb029a5ac
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-05-27 19:53:58 +0900

    detail View Get Image & Init Button

--- Watcha/Views/Search/SearchDetail.swift
+++ Watcha/Views/Search/SearchDetail.swift
@@ -11,16 +11,30 @@ struct SearchDetail: View {
     @EnvironmentObject var searchViewModel: SearchViewModel
     
     var id : String
+
     
     var body: some View {
-        if let index = searchViewModel.items?.all?.firstIndex(where: {$0.id == id}){
-            AsyncImage(url: URL(string : searchViewModel.items!.all![index].images!.original.url)!)
+        VStack{
+            FavoriteButton()
+            if let index = searchViewModel.items?.all!.firstIndex(where: {$0.id == id}){
+                AsyncImage(url: URL(string : searchViewModel.items!.all![index].images!.original.url)!) {phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } else if phase.error != nil {
+                        Text("No Image")
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
         }
     }
 }
 
 struct SearchDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchDetail(id: "" )
+        SearchDetail(id: "")
     }
 }
