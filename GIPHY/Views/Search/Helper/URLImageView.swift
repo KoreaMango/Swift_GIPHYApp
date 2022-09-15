b0961a6745cac8474afa57c4bffe04c4f86641a1
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-15 17:13:43 +0900

    URLImageView Add

---
+++ GIPHY/Views/Search/Helper/URLImageView.swift
@@ -0,0 +1,20 @@
+//
+//  URLImageView.swift
+//  GIPHY
+//
+//  Created by 강민규 on 2022/09/15.
+//
+
+import SwiftUI
+
+struct URLImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct URLImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        URLImageView()
+    }
+}
