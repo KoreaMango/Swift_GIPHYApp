9b04f132d6acfd333d7bc2c86282d0cde183c689
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-09-19 13:22:03 +0900

    disk Cache(userDefaults)

--- GIPHY/Services/DiskCache.swift
+++ GIPHY/Services/DiskCache.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
-class DiskCache: NSObject {
+protocol DiskCacheProtocol {
+    func addData(id : String, isFavorite : Bool)
+    func fetchData(id : String) -> Bool
+}
+
+
+final class DiskCache {
+    private var userDefaults : UserDefaults
     
+    init (userDefaults : UserDefaults = UserDefaults.standard){
+        self.userDefaults = userDefaults
+    }
+}
+
+extension DiskCache : DiskCacheProtocol {
+    /// isFavorite 가져오기
+    func fetchData(id : String) -> Bool {
+        let result = userDefaults.bool(forKey: id)
+        
+        return result
+    }
+    
+    /// isFavorite 저장하기
+    func addData(id : String, isFavorite : Bool) {
+        userDefaults.setValue(isFavorite, forKey: id)
+    }
 }
